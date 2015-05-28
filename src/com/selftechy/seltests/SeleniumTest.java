f3d3ff208d0531bca70aac0e1e7c9576d6dcd778
Maxim Wiens <maxim.wiens@itsd-constulting.de>
2015-05-28 13:40:26 +0200

    Initial commit

---
+++ src/com/selftechy/seltests/SeleniumTest.java
@@ -0,0 +1,82 @@
+package com.selftechy.seltests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import com.thoughtworks.selenium.SeleneseTestBase;
+
+
+
+
+public class SeleniumTest extends SeleneseTestBase {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://itsd-consulting.de/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testSeleneseTestCase() throws Exception {
+    driver.get(baseUrl + "/home.html");
+    driver.findElement(By.cssSelector("h2")).click();
+    driver.findElement(By.xpath("//a[contains(@href, 'consulting_kompetent_nach_mass.html')]")).click();
+    assertEquals("Softwareentwicklung Kompetent und nach Maﬂ", driver.getTitle());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
